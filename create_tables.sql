6f0b114a56ce1cb9fb6b21b78c5c853ae6dc8f59
yaaawner <50330113+yaaawner@users.noreply.github.com>
2021-01-15 18:43:19 +0300

    Members table

---
+++ create_tables.sql
@@ -0,0 +1,26 @@
+USE [Library_2021]
+GO
+
+-- Checking if tables already exist, if so drop them
+IF OBJECT_ID('dbo.Members') IS NOT NULL
+    DROP TABLE [dbo].[Members]
+IF OBJECT_ID('dbo.Books') IS NOT NULL
+    DROP TABLE [dbo].[Books]
+IF OBJECT_ID('dbo.Loanlist') IS NOT NULL
+    DROP TABLE [dbo].[Loanlist]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- Creating Members table
+CREATE TABLE [dbo].[Members](
+	[Member ID] [int] NOT NULL,
+	[Member Name] [nvarchar](max) NOT NULL,
+	[Date] [date] NOT NULL,
+	CONSTRAINT [PK_Members] PRIMARY KEY ([Member ID])
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
