fe0a270e13ed973f3a62e6e8ceb9d100ee553e1d
yaaawner <50330113+yaaawner@users.noreply.github.com>
2021-01-15 19:11:46 +0300

    books

--- create_tables.sql
+++ create_tables.sql
@@ -6,8 +6,8 @@ IF OBJECT_ID('dbo.Members') IS NOT NULL
     DROP TABLE [dbo].[Members]
 IF OBJECT_ID('dbo.Books') IS NOT NULL
     DROP TABLE [dbo].[Books]
-IF OBJECT_ID('dbo.Loanlist') IS NOT NULL
-    DROP TABLE [dbo].[Loanlist]
+IF OBJECT_ID('dbo.Reservation') IS NOT NULL
+    DROP TABLE [dbo].[Reservation]
 GO
 
 SET ANSI_NULLS ON
@@ -22,5 +22,18 @@ CREATE TABLE [dbo].[Members](
 	[Member Name] [nvarchar](max) NOT NULL,
 	[Date] [date] NOT NULL,
 	CONSTRAINT [PK_Members] PRIMARY KEY ([Member ID])
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[Books] (
+	[Book ID] [int] NOT NULL IDENTITY(1,1),
+	[Title] [nvarchar](max) NOT NULL,
+	[Author] [nvarchar](max) NOT NULL,
+	[Year of publication] [int] NOT NULL,
+	[Place of publication] [nvarchar](max) NOT NULL,
+	[Publishing house] [nvarchar](max) NOT NULL,
+	[Total] [int] NOT NULL,
+	[Available] [int] NOT NULL,
+	CONSTRAINT [PK_Books] PRIMARY KEY ([Book ID]),
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
