cd6622327a5750375e4bf22654da4d5fae2305b0
yaaawner <50330113+yaaawner@users.noreply.github.com>
2021-01-18 17:41:34 +0300

    Add files via upload

---
+++ release/LoadDB.sql
@@ -0,0 +1,34 @@
+﻿USE [Library_2021]
+
+EXEC [dbo].[ClearTables]
+
+SET NOCOUNT ON
+GO
+
+SET DATEFORMAT dmy
+GO
+
+INSERT INTO [dbo].[Books] VALUES('Jane Eyre', 'Charlotte Bronte', 2020, 'Moscow', 'AST', 10, 9)
+INSERT INTO [dbo].[Books] VALUES('Powrot Z Gwiazd', 'Stanislav Lem', 2017, 'Moscow', 'AST', 10, 9)
+INSERT INTO [dbo].[Books] VALUES('Star Wars: Darth Bane: Rule Of Two', 'Drew Karpyshyn', 2016, 'Saint-Petersburg', 'Azbuka', 10, 9)
+INSERT INTO [dbo].[Books] VALUES('Star Wars: Darth Bane: Dynasty Of Evil', 'Drew Karpyshyn', 2016, 'Saint-Petersburg', 'Azbuka', 10, 10)
+INSERT INTO [dbo].[Books] VALUES('You Are A Badass', 'Jen Sincero', 2013, 'USA', 'Runnig Press', 10, 10)
+INSERT INTO [dbo].[Books] VALUES('Forecasting: principles and practice', 'Hyndman R. J., Athanasopoulos G.', 2012, 'USA', 'OTexts', 10, 10)
+INSERT INTO [dbo].[Books] VALUES('12 Monkeys', 'Susanne Kord', 2019, 'UK', 'Liverpool University Press', 10, 10)
+INSERT INTO [dbo].[Books] VALUES('Aristotle: On Sleep and Dreams', 'David Gallop', 1996, 'UK', 'Liverpool University Press', 10, 10)
+INSERT INTO [dbo].[Books] VALUES('Brainstorm', 'Joseph Maddrey', 2020, 'UK', 'Liverpool University Press', 10, 10)
+INSERT INTO [dbo].[Books] VALUES('Game Theory and Strategy', 'Philip D. Straffin', 1993, 'USA', 'Mathematical Association of America', 10, 10)
+--INSERT INTO [dbo].[Books] VALUES('12 Monkeys', 'Susanne Kord', 2019, 'UK', 'Liverpool University Press', 10, 10)
+
+INSERT INTO [dbo].[Members] VALUES('Olya Belyaeva', '12-12-2012')
+INSERT INTO [dbo].[Members] VALUES('Dasha Kaverina', '12-12-2012')
+INSERT INTO [dbo].[Members] VALUES('Ksenia Kocheva', '12-12-2012')
+INSERT INTO [dbo].[Members] VALUES('Misha Lalala', '16-01-2021')
+INSERT INTO [dbo].[Members] VALUES('Eva Smith', '16-01-2021')
+
+INSERT INTO [dbo].[Reservation] VALUES(1, 1, '12-12-2020', '20-12-2020', 0)
+INSERT INTO [dbo].[Reservation] VALUES(1, 2, '12-12-2020', '20-12-2020', 0)
+INSERT INTO [dbo].[Reservation] VALUES(1, 3, '12-12-2020', '20-12-2020', 0)
+
+SET NOCOUNT OFF
+GO
---
+++ release/create_tables.sql
@@ -0,0 +1,55 @@
+USE [Library_2021]
+GO
+
+-- Checking if tables already exist, if so drop them
+IF OBJECT_ID('dbo.Reservation') IS NOT NULL
+    DROP TABLE [dbo].[Reservation]
+IF OBJECT_ID('dbo.Members') IS NOT NULL
+    DROP TABLE [dbo].[Members]
+IF OBJECT_ID('dbo.Books') IS NOT NULL
+    DROP TABLE [dbo].[Books]
+
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- Creating Members table
+CREATE TABLE [dbo].[Members](
+	[Member ID] [int] NOT NULL IDENTITY(1,1),
+	[Member Name] [nvarchar](max) NOT NULL,
+	[Date] [date] NOT NULL,
+	CONSTRAINT [PK_Members] PRIMARY KEY ([Member ID])
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[Books] (
+	[Book ID] [int] NOT NULL IDENTITY(1,1),
+	[Title] [nvarchar](max) NOT NULL,
+	[Author] [nvarchar](max) NOT NULL,
+	[Year of publication] [int] NOT NULL,
+	[Place of publication] [nvarchar](max) NOT NULL,
+	[Publishing house] [nvarchar](max) NOT NULL,
+	[Total] [int] NOT NULL,
+	[Available] [int] NOT NULL,
+	CONSTRAINT [PK_Books] PRIMARY KEY ([Book ID]),
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[Reservation] (
+	[Reservation ID] [int] NOT NULL IDENTITY(1,1),
+	[Member ID] [int] NOT NULL,
+	[Book ID] [int] NOT NULL,
+	[Issue date] [date] NOT NULL,
+	[Return date] [date] NOT NULL,
+	[Checkpoint] [bit] NOT NULL, 
+	CONSTRAINT [PK_Reservation] PRIMARY KEY ([Reservation ID]),
+	CONSTRAINT [FK_Reservation_Members] FOREIGN KEY ([Member ID])
+		REFERENCES [dbo].[Members]([Member ID]),
+	CONSTRAINT [FK_Reservation_Books] FOREIGN KEY ([Book ID])
+		REFERENCES [dbo].[Books]([Book ID]),
+) ON [PRIMARY]
+GO
